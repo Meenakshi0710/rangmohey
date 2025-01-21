45e76e1f8502840fcfc8e35ff29aef8da4811277
Meenakshi0710 <meenakshi.motiyani@gmail.com>
2025-01-21 12:17:07 +0530

    2025

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,27 @@ const About = () => {
   return (
    <>
     <Metadata title="About us | RangMohey" />
+   <section className="aboutHero">
+  <img src="/assets/aboutHero.jpg" alt="Hero Section Image" className="img-fluid w-100" />
+  <div className="hero-content d-flex flex-column justify-content-center align-items-center">
+    <div className="row container">
+      <div className="col-md-6 ms-5 col-sm-6 ">
+    <h1 className='display-5 text-center'>Hidden</h1>
+    <h1 className='display-5 text-center'>Gems</h1>
+    <p className='lead text-center'>Embrace tradition with our stunning ethnic wear that celebrate cultural heritage </p>
+  </div>
+  </div>
+  </div>
+</section>
+
   
+
+
+  
+         
+        
+
+   
    </>
   )
 }
