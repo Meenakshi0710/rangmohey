a675ed69fcd45816122ead71ec9ade2459c34700
Meenakshi0710 <meenakshi.motiyani@gmail.com>
2025-06-24 13:23:16 +0530

    correcting

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Navbar2 from './Components/Navbar2/Navbar2';
 import Footer from './Components/Footer/Footer';
-import WhatsAppButton from './Components/WhatsappButton/WhatsappButton';
+import WhatsAppButton from './Components/WhatsappButton/WhatsAppButton';
 
 const Layout = ({ activeSlide }) => {
     
