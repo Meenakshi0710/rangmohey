ffae0ed93076a081b8698324bf99574959919dab
Meenakshi0710 <meenakshi.motiyani@gmail.com>
2025-06-25 11:29:24 +0530

    remove whats app

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Navbar2 from './Components/Navbar2/Navbar2';
 import Footer from './Components/Footer/Footer';
-import WhatsAppButton from './Components/WhatsappButton/WhatsAppButton';
+
 
 const Layout = ({ activeSlide }) => {
     
@@ -10,7 +10,6 @@ const Layout = ({ activeSlide }) => {
     <div className="app">
      <Navbar/>
       <Outlet />
-      <WhatsAppButton/>
       <Footer />
     </div>
   );
